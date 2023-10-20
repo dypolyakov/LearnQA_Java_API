645c64bda2d3f0227b142048983a498348ffa8a5
Dmitry Polyakov <dmitryypolyakov@gmail.com>
2023-10-20 20:19:50 +0500

    Добавлен код из урока 3.4

--- src/test/java/UserAuthTest.java
+++ src/test/java/UserAuthTest.java
@@ -2,7 +2,10 @@ import io.restassured.RestAssured;
 import io.restassured.http.Headers;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -46,7 +49,40 @@ public class UserAuthTest {
 
         assertTrue(userIdOnCheck > 0, "Unexpected user id " + userIdOnCheck);
         assertEquals(userIdOnAuth, userIdOnCheck, "User id from auth request is not equal to user id from check request");
+    }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"cookie", "header"})
+    public void testNegativeAuthUser(String condition) {
+        Map<String, String> authData = new HashMap<>();
+        authData.put("email", "vinkotov@example.com");
+        authData.put("password", "1234");
 
+        Response responseGetAuth = RestAssured
+                .given()
+                .body(authData)
+                .when()
+                .post("https://playground.learnqa.ru/api/user/login")
+                .andReturn();
+
+        Map<String, String> cookies = responseGetAuth.getCookies();
+        Headers headers = responseGetAuth.getHeaders();
+
+        RequestSpecification spec = RestAssured
+                .given()
+                .baseUri("https://playground.learnqa.ru/api/user/auth");
+
+        if (condition.equals("cookie")) {
+            spec.cookie(cookies.get("auth_sid"));
+        } else if (condition.equals("header")) {
+            spec.header("x-csrf-token", headers.getValue("x-csrf-token"));
+        } else {
+            throw new IllegalArgumentException("Condition value is known " + condition);
+        }
+
+        JsonPath responseCheckAuth = spec.get().jsonPath();
+
+        int userIdOnCheck = responseCheckAuth.getInt("user_id");
+        assertEquals(0, userIdOnCheck, "User id should be 0 for unauth request");
     }
 }
